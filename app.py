368c5d1783184e3761a2078aea83fb529deae0b1
paras55 <52483274+paras55@users.noreply.github.com>
2021-06-11 20:53:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app.config.update(
     MAIL_PORT=465,
     MAIL_USE_TLS= False,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'parasmadan123@gmail.com',
-    MAIL_PASSWORD = 'Paras555@'
+    MAIL_USERNAME = '*******',
+    MAIL_PASSWORD = '**********'
 )
 
 
